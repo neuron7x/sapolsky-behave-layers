5d5fb7d2fd16ed7f04ef88a3a42c3fb91fc17fc9
Yaroslav Vasylenko <neuron7x@gmail.com>
2026-08-11 14:07:12 +0000

    fix: break epistemics memory import cycle

--- cwc/epistemics/__init__.py
+++ cwc/epistemics/__init__.py
@@ -18,19 +18,6 @@ from cwc.epistemics.lattice import (
     positive_state_dominates,
 )
 
-from cwc.epistemics.self_falsification import (
-    FalsificationAttack,
-    FalsificationBindingError,
-    FalsificationOutcome,
-    FalsificationUpdate,
-    SelfFalsificationDecision,
-    SelfFalsificationError,
-    SelfFalsificationState,
-    apply_self_falsification_outcome,
-    select_self_falsification_attack,
-    verify_self_falsification_decision,
-)
-
 __all__ = [
     "CapabilityBindingError",
     "CapabilityType",
@@ -47,14 +34,4 @@ __all__ = [
     "POSITIVE_CHAIN",
     "TERMINAL_STATES",
     "positive_state_dominates",
-    "FalsificationAttack",
-    "FalsificationBindingError",
-    "FalsificationOutcome",
-    "FalsificationUpdate",
-    "SelfFalsificationDecision",
-    "SelfFalsificationError",
-    "SelfFalsificationState",
-    "apply_self_falsification_outcome",
-    "select_self_falsification_attack",
-    "verify_self_falsification_decision",
 ]
